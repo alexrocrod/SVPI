5b498f00014fb0fb9bb536d7864785ea686d4c38
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-03-27 17:11:23 +0100

    reorg, start vars

---
+++ TP1/mats/tp1_92993.m
@@ -0,0 +1,35 @@
+% SVPI
+% Alexandre Rodrigues 92993
+% Abril 2022
+% Trabalho Pratico 1
+
+function NumMec = tp1_92993()
+
+%% Init Vars
+    NumMec = 92993;
+    NumSeq = 0;
+    NumImg = 0;
+    tDom = 0;
+    tDice = 0;
+    tCard = 0;
+    RDO = 0;
+    RFO = 0;
+    tDuplas = 0;
+    PntDom = 0;
+    PntDad = 0;
+    CopOuros = 0;
+    EspPaus = 0;
+    Ouros = 0;
+    StringPT = "";
+
+
+%% Open Image
+
+    imshow("../svpi2022_TP1_img_160_01.png")
+
+
+
+
+
+end
+
