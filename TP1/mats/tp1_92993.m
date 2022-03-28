e7df461307b0a07d41aeed28948048bc4c673738
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-03-28 12:59:25 +0100

    aula4 done sor, tp1 com filtros

--- TP1/mats/tp1_92993.m
+++ TP1/mats/tp1_92993.m
@@ -36,14 +36,20 @@ function NumMec = tp1_92993()
 
 %     addpath('../sequencias/Seq350')
 %     listaF=dir('../sequencias/Seq350/svpi2022_TP1_img_*.png');
+    fig1 = figure(1);
+    fig2 = figure(2);
+    fig3 = figure(3);
 
     numFiles = size(listaF,1);
     for idxImg = 1:numFiles
         imName = listaF(idxImg).name;
         NumSeq = str2double(imName(18:20));
         NumImg = str2double(imName(22:23));
+
+        clf(fig1)
    
         A = im2double(imread(imName));
+        figure(fig1)
         imshow(A)
     
         %% SubImages (provisorio)
@@ -51,22 +57,88 @@ function NumMec = tp1_92993()
         regions=vs_getsubimages(A); %extract all regions
         N=numel(regions);
         SS=ceil(sqrt(N));
-        for k=1:N
-            subplot( SS, SS, k)
-            imshow( regions{k} )
+
+        F = zeros(3,3,4);
+        F(:,:,1) = [ 1  1  1;  1  -8  1;  1  1  1];
+        F(:,:,2) = [ 1  2  1;  2 -12  2;  1  2  1];
+        F(:,:,3) = [-1  1 -1;  1   4  1; -1  1 -1];
+        F(:,:,4) = [ 1  2  3;  4 -100 5;  6  7  8];
+        
+        
+        whiteIsol = zeros(3,3); whiteIsol(2,2)=1;
+        w1 = sum(sum(whiteIsol.*F(:,:,:)));
+        W = reshape(w1,1,4);
+        
+        blackIsol = not(whiteIsol);
+        MW1 = sum(sum(blackIsol.*F(:,:,:)));
+        MW = reshape(MW1,1,4);
+
+        n=3;
+        Fiso = F(:,:,n);
+        
+        clf(fig2)
+        clf(fig3)
+        figure(fig2)
+        for k=1:N 
+            figure(fig2)
+            
+%             subplot( SS, SS, k);
+            imshow( regions{k} );
+            B = regions{k};
+
+            temp = filter2(Fiso,B);
+            C = (temp==MW(n));
+            niso = nnz(C);
+
+%             hold on;
+%             [r,c] = find(C);
+%             plot(c,r,'rx') 
+
+            D = (temp==W(n));
+            niso = niso + nnz(D);
+
+%             hold on;
+%             [r,c] = find(D);
+%             plot(c,r,'bx')  
+            
+            str = sprintf('Isolados: %d',niso);
+            xlabel(str);
+            
+            drawnow()
+
+            pause(0.001)
+
+            figure(fig3)
+            hold off
+%             subplot(SS, SS, k)
+            E = zeros(size(B));
+            E(D)=1;
+            E(C)=1;
+            imshow(E)
+            drawnow()
+            pause(1)
+
+            if niso>0
+                disp(niso)
+                pause(2)
+            end
         end
     
         %% Write Table Entry
         T = table(NumMec, NumSeq, NumImg, tDom, tDice, tCard, RDO, ...
             RFO, tDuplas, PntDom, PntDad, CopOuros, EspPaus, Ouros, StringPT);
-        writetable(T,'tp1_92993.txt', 'WriteVariableNames',false, 'WriteMode','append')
+        if idxImg==1
+            writetable(T,'tp1_92993.txt', 'WriteVariableNames',false)
+        else
+            writetable(T,'tp1_92993.txt', 'WriteVariableNames',false, 'WriteMode','append')
+        end
+
+        %% 
+
+
+        %% 
     
-        %% 
-
-
-        %% 
-
-        
+        pause(1)
     end
 
 
