d779b6c24eb1f1c7d439569df7c33cd1cdcdf3cb
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-04-13 09:36:45 +0100

    dados rodados a contar bem exceto os mau hist

--- TP1/mats/testDados.m
+++ TP1/mats/testDados.m
@@ -3,42 +3,40 @@ close all
 
 load("matlab.mat","regions","diceKs")
 
-rodados = [];
+ss = 3;
 for i=1:length(diceKs)
+    figure(i)
     dado1 = regions{diceKs(i)};
 
-    A = strel('diamond',floor(size(dado1,1)/2)+2); %+0
-    dia = A.Neighborhood;
+    subplot(1,ss,1)
+    imshow(dado1);
 
-    [Gmag,Gdir] = imgradient(dado1);
+    subplot(1,ss,2)
+    B = edge(dado1,'log');
+%             B = bwareaopen(B,10);
+    imshow(B)
+    myAxis = axis;
+    hold on, axis ij, axis equal, axis(myAxis), grid on;
+    [L,Nb] = bwlabel(B);
 
-    B = strel('diamond',floor(size(dado1,1)/2)-1); %-2
-    diamin = B.Neighborhood;
-    deltas = size(dia,1)-size(diamin,1);
-    deltas = round(deltas/2);
-    d2 = zeros(size(dia));
-    d2(deltas+1:end-deltas,deltas+1:end-deltas) = diamin;
+    for x = 1:Nb
+        C = (L==x);
+%                 if ( nnz(C) > 2*sx)
+%                     BB = bwboundaries(C,'noholes');
+%                     boundary = BB{1};
+%                 
+%                     plot(boundary(:,2),boundary(:,1),'r');
+%                     continue
+%                 end
+    
+        BB = bwboundaries(C,'noholes');
+        boundary = BB{1};
+    
+        plot(boundary(:,2),boundary(:,1),'b');
+   end
 
-    zona = dia & not(d2);
-    area = nnz(zona);
+    str= sprintf("N=%d\n",Nb);  
+    xlabel(str)
 
-    edges = Gmag>1;
-
-    if nnz(edges(zona(1:size(edges,1),1:size(edges,1)))) > 0.2 * area %.2
-
-        rodadosK = [rodados diceKs(i)];
-
-        
-        A = imrotate(regions{diceKs(i)},45);
-
-        x = size(regions{diceKs(i)},1);
-        l = ceil(x/sqrt(2))+1;
-        deltal = round(l/2)+1;
-        xmeio = round(size(A,1)/2);
-
-
-        regions{diceKs(i)} = A(xmeio-deltal:xmeio+deltal,xmeio-deltal:xmeio+deltal);
-        
-    end
-
+    
 end
