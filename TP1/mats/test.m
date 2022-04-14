dfcad235730ceb55d189c2b4dd3b63be438e6510
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-04-14 16:23:36 +0100

    encontra ouros no 160 img1

--- TP1/mats/test.m
+++ TP1/mats/test.m
@@ -17,25 +17,32 @@ regions2=vs_getsubimages(A); %working
 N=numel(regions);
 SS=ceil(sqrt(N));
 
-figure(1)
-for k=1:N 
-    subplot( SS, SS, k);
-    imshow(regions{k})
-    xlabel(k)
-end
+% figure(1)
+% for k=1:N 
+%     subplot( SS, SS, k);
+%     imshow(regions{k})
+%     xlabel(k)
+% end
 
 
-figure(2)
+% figure(2)
 for k=1:N 
-    subplot( SS, SS, k);
+%     subplot( SS, SS, k);
     regions2{k} = medfilt2(filter2(fspecial('average',3),regions{k}));
-    imshow(regions2{k})
-    xlabel(k)
+%     imshow(regions2{k})
+%     xlabel(k)
 end
 
 
 
 figure(3)
+cartas1k = [];
+cartas2k = [];
+perc = 0.15; % 0.15 
+perc0 = 0.1; % 0.1
+px = 0.15; % 0.14
+py = 0.25; % 0.25
+
 for k=1:N 
     subplot( SS, SS, k);
     cut = 2; % 5
@@ -48,10 +55,11 @@ for k=1:N
     if sx ~= sy
         if sx>sy
             B = rot90(B);
+            regions{k} = rot90(regions{k});
         end
         
-        dx = round(0.14*size(B,1)); % 0.12
-        dy = round(0.25*size(B,2)); % 0.12??
+        dx = round(px*size(B,1));
+        dy = round(py*size(B,2));
         area = dx*dy;
         CantoSupDir = B(1:dx,end-dy:end);
         nnzSupDir = nnz(CantoSupDir);
@@ -62,44 +70,159 @@ for k=1:N
         CantoInfEsq = B(end-dx:end,1:dy);
         nnzInfEsq = nnz(CantoInfEsq);
 
-%         cIESD = ;
-        perc = 0.15;
-        perc0 = 0.1;
         if (nnzInfEsq > perc*area && nnzSupDir > perc*area  && ...
                 nnzInfDir < perc0*area && nnzSupEsq < perc0*area)
-%             Tapar os q nao tem nada
-            B(1:dx,1:dy) = 1;
-            B(end-dx:end,end-dy:end) = 1;
+% %             Tapar os q nao tem nada
+%             B(1:dx,1:dy) = 1;
+%             B(end-dx:end,end-dy:end) = 1;
+            B(1:dx,1:dy) = 0;
+            B(end-dx:end,end-dy:end) = 0;
+
             imshow(B)
             xlabel("carta 1")
+            cartas1k = [cartas1k k];
             
         elseif (nnzInfDir > perc*area && nnzSupEsq > perc*area && ...
                 nnzInfEsq < perc0*area && nnzSupDir < perc0*area)
-            B(end-dx:end,1:dy) = 1;
-            B(1:dx,end-dy:end) = 1;
+            B(end-dx:end,1:dy) = 0;
+            B(1:dx,end-dy:end) = 0;
             imshow(B)
             xlabel("carta 2")
+            cartas2k = [cartas2k k];
         else
             imshow(B)
         end
-        
-        
-
-        
     
-        
     else
         imshow(B)
     end
 
+    regions2{k} = B;
+
 end
+
+
+figure(4)
+idx = 1;
+cut = 2; % 5
+nrows = 4;
+means = zeros(length(cartas1k),1);
+for k=cartas1k
+    
+%     B = regions2{k};
+%     B = regions{k};
+    
+    B = regions{k}(cut:end-cut,cut:end-cut);
+    
+    sx = size(B,1);
+    sy = size(B,2);
+
+    dx = round(px*size(B,1)); % 0.14
+    dy = round(py*size(B,2)); % 0.25??
+    CantoSupDir = rot90(B(1:dx,end-dy:end));
+    CantoInfEsq = rot90(B(end-dx:end,1:dy));
+
+
+    subplot(nrows, length(cartas1k), idx);
+    imshow(CantoSupDir)
+    
+
+%     subplot(nrows, length(cartas1k), idx+length(cartas1k));
+%     imshow(CantoInfEsq)
+
+    CSDbin = autobin(imadjust(CantoSupDir));
+    CIEbin = autobin(imadjust(CantoInfEsq));
+
+%     subplot(nrows, length(cartas1k), idx+length(cartas1k));
+%     imshow(CSDbin)
+
+%     subplot(nrows, length(cartas1k), idx+length(cartas1k)*3);
+%     imshow(CIEbin)
+    
+    dx2 = round(0.55*size(CSDbin,1)); 
+    NaipeSD = CSDbin(dx2:end,:);
+    
+    
+    subplot(nrows, length(cartas1k), idx+length(cartas1k));
+    imshow(NaipeSD)
+
+%     edgeNaipe = edging(NaipeSD);
+%     subplot(nrows, length(cartas1k), idx+length(cartas1k)*3);
+%     imshow(edgeNaipe)
+
+%     ouros = strel('diamond',)
+    clean0s = NaipeSD(any(NaipeSD,2),:);
+    clean0s = clean0s(:,any(clean0s,1));
+    subplot(nrows, length(cartas1k), idx+length(cartas1k)*2);
+    imshow(clean0s)
+   
+    ouro1 = strel('diamond',ceil(0.5*size(clean0s,1))).Neighborhood;
+    ouro1 = imresize(ouro1,size(clean0s));
+    subplot(nrows, length(cartas1k), idx+length(cartas1k)*3);
+    imshow(ouro1)
+    
+    means(idx) = mean(imresize(clean0s,10)~=imresize(ouro1,10),'all');
+    if means(idx) < 1e-1
+        xlabel("Ouros")
+    else
+        xlabel(sprintf("Ndif:%d",means(idx)))
+    end
+    
+
+    idx = idx + 1;
+
+end
+
+if not(isempty(cartas2k))
+    figure(5)
+    idx = 1;
+    for k=cartas2k
+    
+    %     B = regions2{k};
+    %     B = regions{k};
+        B = regions{k}(cut:end-cut,cut:end-cut);
+        
+        sx = size(B,1);
+        sy = size(B,2);
+    
+        dx = round(px*size(B,1)); % 0.14
+        dy = round(py*size(B,2)); % 0.25??
+        
+        CantoInfDir = B(end-dx:end,end-dy:end);
+        CantoSupEsq = B(1:dx,1:dy);
+    
+        subplot(4, length(cartas2k), idx);
+        imshow(CantoInfDir)
+    
+        subplot(4, length(cartas2k), idx+length(cartas2k));
+        imshow(CantoSupEsq)
+
+        CSEbin = autobin(imadjust(CantoSupEsq));
+        CIDbin = autobin(imadjust(CantoInfDir));
+
+        subplot(4, length(cartas2k), idx+length(cartas2k)*2);
+        imshow(CIDbin)
+    
+        subplot(4, length(cartas2k), idx+length(cartas2k)*3);
+        imshow(CSEbin)
+            
+    
+        idx = idx + 1;
+    
+        
+    
+    end
+end
+
+% deck = ["♠","♥","♦","♣"] + ["A"; (2:10)';'J';'Q';'K'];
 
 function B = edging(A)
     B = A;
 %     B = medfilt2(B);
     B = edge(B,'roberts');
-    B = bwareaopen(B,round(0.5*size(B,1)));
-    B = bwmorph(B,'close');
+%     B = bwareaopen(B,round(0.1*size(B,1)));
+    B = bwmorph(B,'remove');
+    B = bwmorph(B,'bridge');
 %     B = bwmorph(B,'fill');
     
 %     B = bwmorph(B,'remove'); % remove
