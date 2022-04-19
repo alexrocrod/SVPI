059e84bf325d8ef471056891e7bfa79d151c629b
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-04-19 15:38:20 +0100

    roda os dados, reduz pxCentroCarta

--- TP1/mats/compares.m
+++ TP1/mats/compares.m
@@ -13,25 +13,20 @@ A_exact530 = readtable("tp1_seq_530_results.txt");
 B = readtable("tp1_92993.txt");
 
 B160 = B(1:40,:);
-B530 = B(41:end,:);
-
-
-
-
 Diff160 = table2array(B160)-table2array(A_exact160);
-
-Diff530 = table2array(B530)-table2array(A_exact530);
-
 Erro160 = Diff160./table2array(A_exact160)*100;
-
-Erro530 = Diff530./table2array(A_exact530)*100;
-
-
 MediaErros160 = mean(Erro160,1);
 MediaErrosImp160 = mean(Erro160(:,2:end-1),1);
 
+
+
+B530 = B(41:end,:);
+Diff530 = table2array(B530)-table2array(A_exact530);
+Erro530 = Diff530./table2array(A_exact530)*100;
 MediaErros530 = mean(Erro530,1);
 MediaErrosImp530 = mean(Erro530(:,2:end-1),1);
+
+
 
 % T = table(NumMec, NumSeq, NumImg, tDom, tDice, tCard, RDO, RFO, tDuplas, PntDom, PntDad, CopOuros, EspPaus, Ouros, StringPT);
 % Tcut = table(NumSeq, NumImg, tDom, tDice, tCard, RDO, RFO, tDuplas, PntDom, PntDad, CopOuros, EspPaus, Ouros);
---
+++ TP1/mats/untitled2.m
@@ -0,0 +1,90 @@
+idxImg=1
+idxImg=2
+idxImg=3
+idxImg=4
+idxImg=5
+idxImg=6
+idxImg=7
+idxImg=8
+idxImg=9
+idxImg=10
+idxImg=11
+idxImg=12
+idxImg=13
+idxImg=14
+idxImg=15
+idxImg=16
+idxImg=17
+idxImg=18
+idxImg=19
+idxImg=20
+idxImg=21
+idxImg=22
+idxImg=23
+idxImg=24
+idxImg=25
+idxImg=26
+idxImg=27
+idxImg=28
+idxImg=29
+Rodado 38
+idxImg=30
+idxImg=31
+idxImg=32
+idxImg=33
+idxImg=34
+idxImg=35
+idxImg=36
+idxImg=37
+idxImg=38
+idxImg=39
+idxImg=40
+
+ans =
+
+       92993
+
+
+idxImg=1
+idxImg=2
+idxImg=3
+Carta NA, k=18
+idxImg=4
+Carta NA, k=15
+Carta NA, k=33
+idxImg=5
+Carta NA, k=34
+idxImg=6
+Remove Carta:Nb=0
+idxImg=7
+Remove Carta:Nb=0
+idxImg=8
+idxImg=9
+idxImg=10
+Remove Carta:Nb=0
+idxImg=11
+Remove Carta:Nb=0
+idxImg=12
+IdxImg:12,Removeu rodado 4, Nb:7
+idxImg=13
+idxImg=14
+Carta NA, k=19
+idxImg=15
+idxImg=16
+Remove Carta:Nb=0
+idxImg=17
+idxImg=18
+Carta NA, k=9
+Remove Carta:Nb=0
+idxImg=19
+Rodado 22
+Removeu rodado 22, Nb:0
+idxImg=20
+Carta NA, k=12
+
+ans =
+
+       92993
+
+
+
