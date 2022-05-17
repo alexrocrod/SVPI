2103f545f9a2aebff089ef6525654e54a007efa0
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-05-17 16:25:01 +0100

    class im01 so com 6 erros, falta partidas e etc

--- TP2/mats/classify.m
+++ TP2/mats/classify.m
@@ -21,6 +21,7 @@ figure;
 for k=1:N
     subplot(SS,SS,k)
     imshow(regionsRef{k})
+    regionsRef{k} = logical(regionsRef{k});
     regionsGray{k} = rgb2gray(regionsRGBRef{k});
     xlabel(k)
 end
@@ -65,23 +66,70 @@ for k=1:N
     Brgb = regionsRGB{k};
     Bbin = B;
     Bbin = bwareaopen(Bbin,10);
+%     figure;
+%     subplot(1,4,1)
+%     imshow(Brgb)
+    oriB = regionprops(Bbin,'Orientation').Orientation;
     for iRef=1:Nref
-%     for iRef=1:2:Nref-1
-%         size(getFeats(B,Bbin,nFeats,1,0))
-%         size(AllFeatures{iRef})
-        dist(k,iRef) = mahal(getFeats(Brgb,B,Bbin,nFeats,1,0)',AllFeatures{iRef}');
-%         ola = getFeatures({B},{Brgb},nFeats);
-%         dist(k,iRef) = mean(mahal(ola{1}',AllFeatures{iRef}'));
-%         dist(k,iRef) = min(mahal(ola{1}',AllFeatures{iRef}'));
-%         dist(k,iRef+1) = dist(k,iRef);
-%         pause(0.1)
+        oriRef = regionprops(regionsRef{iRef},'Orientation').Orientation;
+%         subplot(1,4,2)
+%         imshow(regionsRGBRef{iRef})
+
+        Brgb2 = imrotate(Brgb,oriRef-oriB);
+        Bbin2 = imrotate(Bbin,oriRef-oriB);
+        B2 = imrotate(B,oriRef-oriB);
+
+        Bbin2 = Bbin2(:,any(Bbin2,1));
+        Bbin2 = Bbin2(any(Bbin2,2),:);
+        
+        Brgb2 = Brgb2(:,any(B2,1),:);
+        Brgb2 = Brgb2(any(B2,2),:,:);
+
+        B2 = B2(:,any(B2,1));
+        B2 = B2(any(B2,2),:);
+
+%         Brgb2 = imresize(Brgb2,size(regionsRef{iRef}));
+%         Bbin2 = imresize(Bbin2,size(regionsRef{iRef}));
+%         B2 = imresize(B2,size(regionsRef{iRef}));
+        Brgb2 = imresize(Brgb2,[size(regionsRef{iRef},1) NaN]);
+        Bbin2 = imresize(Bbin2,[size(regionsRef{iRef},1) NaN]);
+        B2 = imresize(B2,[size(regionsRef{iRef},1) NaN]);
+
+        Bbin2 = bwareaopen(Bbin2,size(regionsRef{iRef},1));
+
+        [~,Nb] = bwlabel(Bbin2);
+        if Nb==0
+            disp("fail")
+            Brgb2 = Brgb;
+            Bbin2 = Bbin;
+            B2 = B;
+        end
+
+%         subplot(1,4,3)
+%         imshow(Brgb2)
+%         oriB2 = regionprops(Bbin2,'Orientation').Orientation;
+        dists(1) = norm((getFeats(Brgb2,B2,Bbin2,nFeats)-AllFeatures(:,iRef)));
+
+        Brgb3 = imrotate(Brgb2,180);
+        Bbin3 = imrotate(Bbin2,180);
+        B3 = imrotate(B2,180);
+
+        [L,Nb] = bwlabel(Bbin3);
+        if Nb==0
+            disp("fail")
+            Brgb3 = Brgb;
+            Bbin3 = Bbin;
+            B3 = B;
+        end
+
+%         subplot(1,4,4)
+%         imshow(Brgb3)
+%         oriB2 = regionprops(Bbin2,'Orientation').Orientation;
+        dists(2) = norm((getFeats(Brgb3,B3,Bbin3,nFeats)-AllFeatures(:,iRef)));
+        dist(k,iRef) = min(dists);
+%         pause(1)
     end
 end
-%%
-% ola = getFeatures({B},{Brgb},nFeats);
-% size(ola)
-% size(ola{1}')
-% size(AllFeatures{iRef}')
 
 
 %%
@@ -104,120 +152,57 @@ for k=1:N
     
     pause(0.1)
 end
-%%
 
 
 %%
-function AllFeatures = getFeatures(regions,regionsGray,regionsRGB,nFeats)
+ola = getFeats(Brgb2,B2,Bbin2,nFeats);
+size(ola)
+size(ola')
+size(AllFeatures(:,iRef)')
+
+figure;
+subplot(1,4,1)
+imshow(Brgb)
+subplot(1,4,2)
+imshow(regionsRGBRef{iRef})
+subplot(1,4,3)
+imshow(Brgb2)
+subplot(1,4,4)
+imshow(Brgb3)
+
+
+%%
+function features = getFeatures(regions,regionsGray,regionsRGB,nFeats)
     N = numel(regions);
-    features = zeros(nFeats,24);
-%     features = zeros(nFeats,2);
+    features = zeros(nFeats,N);
     for k=1:N
         A = regionsGray{k};
         Argb = regionsRGB{k};
         Abin = regions{k};
-        idx = 1;
-        for rot=0:45:315
-            for sc=0.5:0.5:1.5
-                A2 = imresize(imrotate(Abin,rot),sc);
-                A2 = bwareaopen(A2,10);
-                Agray2 = imresize(imrotate(A,rot),sc);
-                Argb2 = imresize(imrotate(Argb,rot),sc);
-                features(:,idx) = getFeats(Argb2,Agray2,A2,nFeats,sc,rot);
-                idx = idx + 1;
-            end
-        end
-        AllFeatures{k} = features;
+        features(:,k) = getFeats(Argb,A,Abin,nFeats);
     end
     
 end
 
-function feats = getFeats(ARGB,Agray,Abin,nFeats,sc,angle)
-    if nFeats == 14
-        s = regionprops(Abin,'basic');
-        feats = [invmoments(Agray) s.Area s.Centroid s.BoundingBox]';
-%     elseif nFeats== 7
-%         s = regionprops(Abin,'All');
-%         feats = [s.Area/sc/sc s.MajorAxisLength/sc s.MinorAxisLength./sc s.Eccentricity mod(mod(s.Orientation,180)+angle,90)  s.Circularity s.EquivDiameter/sc  s.MaxFeretDiameter/sc s.MinFeretDiameter/sc]';
-%     elseif nFeats == 10
-%         s = regionprops(Abin,'Circularity','Eccentricity','EulerNumber');
-%         feats = [invmoments(Agray) s.Circularity s.Eccentricity s.EulerNumber]';
-    elseif nFeats == 10
+function feats = getFeats(ARGB,Agray,Abin,nFeats)
+%     if nFeats == 12
         s = regionprops(Abin,'Circularity','Eccentricity','Solidity');
-        ola = real(log(invmoments(Agray)));
-%         ola = abs(ola)/max(abs(ola));
-        feats = [ola s.Circularity s.Eccentricity s.Solidity]';
-    elseif nFeats == 12
-        s = regionprops(Abin,'Circularity','Eccentricity','Solidity');
-        R = ARGB(:,:,1);
-        meanR = mean(R(Abin),'all');
-        G = ARGB(:,:,2);
-        meanG = mean(G(Abin),'all');
-        B = ARGB(:,:,3);
-        meanB = mean(B(Abin),'all');
-%         R = ARGB(:,:,1);
-%         meanR = mean(R,'all');
-%         G = ARGB(:,:,2);
-%         meanG = mean(G,'all');
-%         B = ARGB(:,:,3);
-%         meanB = mean(B,'all');
+        meanR = mean(ARGB(:,:,1),'all');
+        meanG = mean(ARGB(:,:,2),'all');
+        meanB = mean(ARGB(:,:,3),'all');
         ola = real(log(invmoments(Agray)));
         feats = [meanR meanG meanB ola s.Eccentricity s.Solidity]';
-    elseif false %nFeats == 12
-        s = regionprops(Abin,'All');
-        areaFig = size(Abin,1)*size(Abin,2)/1e3;
-        maxS = max(size(Abin));
-        minS = min(size(Abin));
-        R = ARGB(:,:,1);
-        meanR = mean(R(Abin),'all');
-        G = ARGB(:,:,2);
-        meanG = mean(G(Abin),'all');
-        B = ARGB(:,:,3);
-        meanB = mean(B(Abin),'all');
-%         feats = [meanR meanG meanB s.Solidity s.Area./areaFig./1000 s.MajorAxisLength./areaFig s.MinorAxisLength./areaFig s.Eccentricity s.Circularity s.EquivDiameter/areaFig s.MaxFeretDiameter/areaFig s.MinFeretDiameter/areaFig]';
-        feats = [meanR meanG meanB s.Solidity s.Area./areaFig./1000 s.MajorAxisLength./maxS s.MinorAxisLength./minS s.Eccentricity s.Circularity s.EquivDiameter/maxS s.MaxFeretDiameter/maxS s.MinFeretDiameter/minS]';
-%     elseif nFeats == 10
-%         s = regionprops(Abin,'All');
-%         areaFig = size(Abin,1)*size(Abin,2)/1e3;
-%         meanGray = mean(Agray(Abin),'all');
-%         feats = [meanGray s.Solidity s.Area/areaFig/1000 s.MajorAxisLength/areaFig s.MinorAxisLength./areaFig s.Eccentricity s.Circularity s.EquivDiameter/areaFig s.MaxFeretDiameter/areaFig s.MinFeretDiameter/areaFig]';
-    elseif nFeats == 2
-        s = regionprops(Abin,'Circularity','Eccentricity');
-        feats = [s.Circularity s.Eccentricity]';
-    else
-        s = regionprops(Abin,'All');
-%         MeanFA = (s.MaxFeretAngle-s.MinFeretAngle)/sc??;
-%         feats = [s.Area/sc/sc s.MajorAxisLength/sc s.MinorAxisLength./sc s.Eccentricity mod(mod(s.Orientation,180)+angle,90)  s.Circularity s.EquivDiameter/sc  s.MaxFeretDiameter/sc s.MinFeretDiameter/sc]'; %s.Perimeter/sc
-        areaFig = size(Abin,1)*size(Abin,2)/1e3;
-%         maxS = max(size(Abin));
-%         minS = min(size(Abin));
-%         feats = [s.Area/areaFig s.MajorAxisLength/areaFig s.MinorAxisLength./areaFig s.Eccentricity mod(mod(s.Orientation,180)+angle,90)/90  s.Circularity s.EquivDiameter/areaFig s.MaxFeretDiameter/areaFig s.MinFeretDiameter/areaFig]'; %s.Perimeter/sc       
-%         feats = [s.Area/areaFig/1000 s.MajorAxisLength/areaFig s.MinorAxisLength./areaFig s.Eccentricity s.Circularity s.EquivDiameter/areaFig s.MaxFeretDiameter/areaFig s.MinFeretDiameter/areaFig]'; %s.Perimeter/sc       
-        feats = [s.Solidity s.Area/areaFig/1000 s.MajorAxisLength/areaFig s.MinorAxisLength./areaFig s.Eccentricity s.Circularity s.EquivDiameter/areaFig s.MaxFeretDiameter/areaFig s.MinFeretDiameter/areaFig]'; %s.Perimeter/sc 
-%         feats = [s.Solidity s.Area/areaFig s.MajorAxisLength/maxS s.MinorAxisLength./minS s.Eccentricity s.Circularity s.EquivDiameter/maxS s.MaxFeretDiameter/maxS s.MinFeretDiameter/minS]'; %s.Perimeter/sc 
-        %algumas coisas nao escalam com areaFig so...
-    end
+%     end
 end
 
 
 function [regions,regionsRGB,fullMask,countBord] = getSubImages(A,minSize,cutx,cuty,relSizes,minWidth,extend,fmaskPrev,imgRef,minAreaMigalha)
-
     % get all subimages(regions)
-
-%     B = maskNormal(A);
-%     
-%     B = bwareaopen(B,round(minSize*size(B,1)));
-%         B = medfilt2(filter2(fspecial("average",3),A));
 
     B = A;
     T = adaptthresh(B);
 
     E = imbinarize(A,T);
-
-%     mean(E,'all');
-
-
-%     Eold = E;
 
     if mean(E,'all') > 0.3
         E = not(E);
